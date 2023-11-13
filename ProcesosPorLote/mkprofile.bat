a23cec368a16171807fcee8533bb385a3919b4ef
JuaanCarlos <150673262+JuaanCarlos@users.noreply.github.com>
2023-11-13 12:20:01 +0100

    Add files via upload

---
+++ ProcesosPorLote/mkprofile.bat
@@ -0,0 +1,11 @@
+@echo off
+REM Primer ejercicio de script
+REM
+REM Autor: Juan Carlos Sánchez
+REM Fecha: 13-11-2023
+mkdir %USERPROFILE%\asir %USERPROFILE%\dam %USERPROFILE%\daw %USERPROFILE%\smr %USERPROFILE%\ceti
+mkdir %USERPROFILE%\asir/pract %USERPROFILE%\asir/docs %USERPROFILE%\asir/exam %USERPROFILE%\asir\ceti
+mkdir %USERPROFILE%\dam/pract %USERPROFILE%\dam/docs %USERPROFILE%\dam\exam %USERPROFILE%\dam\ceti
+mkdir %USERPROFILE%\daw\pract %USERPROFILE%\daw\docs %USERPROFILE%\daw\exam %USERPROFILE%\daw\ceti
+mkdir %USERPROFILE%\smr\pract %USERPROFILE%\smr\docs %USERPROFILE%\smr\exam %USERPROFILE%\smr \ceti
+mkdir %USERPROFILE%\ceti\pract %USERPROFILE%\ceti\docs %USERPROFILE%\ceti\exam %USERPROFILE%\ceti\ceti
